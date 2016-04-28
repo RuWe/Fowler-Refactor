33bfeb056df6d994fdd77926415144bf303cb3cc
RuWe <Ruthweber95@web.de>
2016-04-28 22:42:57 +0200

    STEP: Replace Conditional with Polymorphism
    The conditional are moved to an overriding method in the subclass.

--- src/Movie.java
+++ src/Movie.java
@@ -43,11 +43,7 @@ public class Movie {
     }
 
     public int getFrequentRenterPoints(int daysRented) {
-        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
-            return 2;
-        } else {
-            return 1;
-        }
+        return this.price.getFrequentRenterPoints(daysRented);
     }
 
 }
